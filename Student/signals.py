d1e383e000d351efd1aac0bbc4f2d0f7881cdd1f
Trevor K. Jemmott Jr <tafittechtt@gmail.com>
2023-11-09 10:57:17 -0400

    commit

--- Student/signals.py
+++ Student/signals.py
@@ -3,8 +3,18 @@ from django.dispatch import receiver
 
 
 from Admin.models import StudentProfile
+from .models import StudentRegistration
 
-
+@receiver(post_save, sender= StudentRegistration)  
+def studentRegistration(sender, instance ,created, *args, **kwargs ):
+    profile = instance
+    user    = profile.studentprofile
+    if created == False:
+        user.email = profile.email
+        user.address = profile.address
+        user.phone   =  profile.phone
+        user.mobile  =  profile.mobile 
+        user.save( ) 
 
 
 @receiver(post_save, sender= StudentProfile)  
