9878700c25b24878a6a50f40ccf2c76edd8e1bae
Trevor K. Jemmott Jr <tafittechtt@gmail.com>
2023-11-09 18:22:08 -0400

    commit

--- Admin/models.py
+++ Admin/models.py
@@ -135,5 +135,17 @@ class AdminProfile(models.Model):
 
 
 
+class Messages(models.Model):
+    sender =models.ForeignKey(AdminProfile, on_delete=models.SET_NULL,null=True, Blank=True)
+    recipient =models.ForeignKey(AdminProfile, on_delete=models.SET_NULL,null=True, Blank=True, related_name='messages' )
+    name = models.CharField(max_length=255,blank=True , null=True)
+    email = models.EmailField(max_length=255,blank=True , null=True)
+    subject = models.CharField(max_length=255,blank=True , null=True)
+    body = models.TextField()
+    is_read = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.subject
+    
     
