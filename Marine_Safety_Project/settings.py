a8a6a7ed1f4b681afc55c8473712d06b392b3492
Trevor K. Jemmott Jr <tafittechtt@gmail.com>
2023-11-15 17:11:38 -0400

    commit

--- Marine_Safety_Project/settings.py
+++ Marine_Safety_Project/settings.py
@@ -107,6 +107,13 @@ USE_I18N = True
 USE_TZ = True
 
 
+EMAIL_BACKEND ='django.core.mail.backend.smtp.EmailBackend'
+EMAIL_HOST    = 'smtp.gmail.com'
+EMAIL_PORT    = '587'
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'tafitinvestmentgrouptt@gmail.com'
+EMAIL_HOST_PASSWORD = 'Trevo4Je2mo2t1@'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
