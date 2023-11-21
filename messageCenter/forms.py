b90bb13952ab26f025a38a08333cdcd0394780fc
Trevor K Jemmott <tafittechtt@gmail.com>
2023-11-21 04:53:46 -0400

    message form created

---
+++ messageCenter/forms.py
@@ -0,0 +1,23 @@
+from django.forms  import ModelForm
+from django   import forms
+
+from .models  import Message
+
+
+class Message_Form(ModelForm):
+    class Meta:
+        model = Message
+        fields= [
+            'name', 'email', 
+            'subject', 'body',
+        ]
+        labels = {
+            'name':'Full Name', 'email':'Email', 
+            'subject':'Subject', 'body':'Message',
+        }
+    
+    def __init__(self, *args,**kwargs):
+            super(Message_Form, self).__init__(*args,*kwargs)
+
+            for name, field in self.fields.items():
+                field.widget.attrs.update({'class':'input-text'})
