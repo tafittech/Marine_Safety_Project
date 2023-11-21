f9d54378c0f6f1da919c1a94ea1a382333769c01
Trevor K Jemmott <tafittechtt@gmail.com>
2023-11-21 04:37:55 -0400

    model message created

--- messageCenter/models.py
+++ messageCenter/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+
+from Admin.models import User
+
 # Create your models here.
+class Message(models.Model):
+    sender      =models.ForeignKey(User,on_delete=models.SET_NULL,null=True, blank=True)
+    recipient   =models.ForeignKey(User,on_delete=models.SET_NULL,null=True, blank=True,related_name='messages' )
+    name        = models.CharField(max_length=255,blank=True , null=True)
+    email       = models.EmailField(max_length=255,blank=True , null=True)
+    subject     = models.CharField(max_length=255,blank=True , null=True)
+    body        = models.TextField()
+    is_read     = models.BooleanField(default=False, null=True)
+    created     = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.subject
+
+    
+    class Meta:
+        ordering =['is_read','-created']
