a61abd9893490aa33b81c964240aed692b2684e8
Trevor K Jemmott <tafittechtt@gmail.com>
2023-11-21 05:33:55 -0400

    message center inbox complete

--- messageCenter/views.py
+++ messageCenter/views.py
@@ -1,6 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
+
+
+from Admin.models import User
+from .models import Message
+from .forms import Message_Form
 # Create your views here.
-
+@login_required(login_url='student-login')
 def messageCenter(request):
-    return render(request, 'messageCenter.html', {})
+    messageRequest = Message.objects.all()
+    unreadCount = messageRequest.filter(is_read=False).count()
+    context ={ 
+        'messageRequest':messageRequest, 'unreadCount':unreadCount
+    }
+    return render(request, 'messageCenter.html', context)
+
+
+
+
+
+
+
+    
+
+
+
