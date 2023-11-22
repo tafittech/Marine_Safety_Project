92255824473877e1d7d0412fafdfaf967dfd419b
Trevor K. Jemmott Jr <tafittechtt@gmail.com>
2023-11-22 18:03:59 -0400

    course model update

--- Course/models.py
+++ Course/models.py
@@ -70,3 +70,25 @@ class AttendanceReport(models.Model):
         return self.status
 
 
+
+class NotificationStudent(models.Model):
+    id            = models.AutoField(primary_key=True)
+    student_id    = models.ForeignKey(StudentProfile, on_delete=models.CASCADE)
+    message       = models.TextField()
+    created_id    = models. DateTimeField(auto_now_add=True)
+    updated_id    = models. DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.message
+
+
+
+class NotificationStaff(models.Model):
+    id            = models.AutoField(primary_key=True)
+    staff_id      = models.ForeignKey(AdminProfile, on_delete=models.CASCADE)
+    message       = models.TextField()
+    created_id    = models. DateTimeField(auto_now_add=True)
+    updated_id    = models. DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.message
