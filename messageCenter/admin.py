7fb863e8d91d270fd699e617d37f61f392b54fef
Trevor K Jemmott <tafittechtt@gmail.com>
2023-11-27 11:21:28 -0400

    just commit

--- messageCenter/admin.py
+++ messageCenter/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
 
-from .models import Message
+from .models import (
+    Message, AdminMessage
+) 
 
 # Register your models here.
-
+admin.site.register(AdminMessage)
 admin.site.register(Message)
