0be8154d8e041a5b03a9d3f6b70cbefab3158582
Trevor K. Jemmott Jr <tafittechtt@gmail.com>
2023-12-12 12:14:34 -0400

    moffice update

--- Course/views.py
+++ Course/views.py
@@ -5,12 +5,15 @@ from django.shortcuts import render, redirect
 from .models import (
     Course
 )
+from .forms import (
+    addCourse_Form
+)
 
 # Create your views here.
 
 def addCourse(request):
-    course = ad
-    return render(request, 'add_course.html',{})
+    course = addCourse_Form()
+    return render(request, 'add_course.html',{'form':course})
 
 def addCourseSave(request):
    pass
